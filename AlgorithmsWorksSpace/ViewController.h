3070e2785817ff692e3b2c31f6665ec94a1f1a32
Deepak Kumar <DeepakKumar@Deepaks-MacBook-Pro.local>
2014-11-16 17:19:59 +0530

    Initial Commit

---
+++ AlgorithmsWorksSpace/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AlgorithmsWorksSpace
+//
+//  Created by Deepak Kumar on 16/11/14.
+//  Copyright (c) 2014 Deepak Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AlgorithmsWorksSpace/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AlgorithmsWorksSpace
+//
+//  Created by Deepak Kumar on 16/11/14.
+//  Copyright (c) 2014 Deepak Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
